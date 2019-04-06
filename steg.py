10dbd462bce40bad54eaf09fc01de7553e3330ce
Jonas <201600057@post.au.dk>
2019-04-06 16:00:54 +0200

    Dynamic functions incoming

--- steg.py
+++ steg.py
@@ -1,7 +1,32 @@
 import numpy as np
 import cv2
+import binascii
 from scipy.fftpack import fft, dct
 import matplotlib.pyplot as plt
+
+def msgToEncode(data, msg):
+    
+    binString = ' '.join(format(ord(x), 'b') for x in msg)
+
+    prevWord = 0
+    iterator = 0
+    
+    for x in range(0, len(binString), 8):
+        currWord = binString[prevWord:x]
+        print(currWord)
+        prevWord = x
+        
+        msb = "{0:b}".format(int(enc[iterator,0]))
+        enc[iterator,0] = int(msb1[:-8] + currWord, 2)
+        
+    msb1 = "{0:b}".format(int(enc[0,0]))
+    # adjust the 2 lsb, 01001000 = H in ascii
+    
+    
+    #decode the encoded picture
+    return dec = dct(enc,3)    
+    
+    return;
 
 fn = 'yoda_lego.jpg'
 
@@ -12,24 +37,27 @@ plt.gray()
 plt.show()
 
 
-enc = dct(img,2)
+enc = dct(img,2) # dct encode
 
+# extract values for the first four pixels
 msb1 = "{0:b}".format(int(enc[0,0]))
-msb2 = "{0:b}".format(int(enc[1,0]))
-msb3 = "{0:b}".format(int(enc[2,0]))
-msb4 = "{0:b}".format(int(enc[3,0]))
+#msb2 = "{0:b}".format(int(enc[1,0]))
+#msb3 = "{0:b}".format(int(enc[2,0]))
+#msb4 = "{0:b}".format(int(enc[3,0]))
 
+#print the shit
 print()
 print(enc[0,0])
-print(enc[1,0])
-print(enc[2,0])
-print(enc[3,0])
+#print(enc[1,0])
+#print(enc[2,0])
+#print(enc[3,0])
 print()
 
-enc[0,0] = int(msb1[:-2] + '01', 2)
-enc[1,0] = int(msb2[:-2] + '00', 2)
-enc[2,0] = int(msb3[:-2] + '10', 2)
-enc[3,0] = int(msb4[:-2] + '00', 2)
+# adjust the 2 lsb, 01001000 = H in ascii
+enc[0,0] = int(msb1[:-8] + '01001000', 2)
+# enc[1,0] = int(msb2[:-2] + '00', 2)#
+#enc[1,0] = int(msb2[:-4] + '1000', 2)
+#enc[3,0] = int(msb4[:-2] + '00', 2)
 
 # print("{0:b}".format(int(enc[0,0])))
 # print("{0:b}".format(int(enc[1,0])))
@@ -37,19 +65,20 @@ enc[3,0] = int(msb4[:-2] + '00', 2)
 # print("{0:b}".format(int(enc[3,0])))
 print()
 print(enc[0,0])
-print(enc[1,0])
-print(enc[2,0])
-print(enc[3,0])
+#print(enc[1,0])
+#print(enc[2,0])
+#print(enc[3,0])
 print()
 
-
+#decode the encoded picture
 dec = dct(enc,3)
 
+fig = plt.figure()
 plt.imshow(dec)
 plt.gray()
 plt.show()
 
-enc2 = dct(dec,2)
+enc2 = dct(dec,2)/850
 
 # print("{0:b}".format(int(enc2[0,0])))
 # print("{0:b}".format(int(enc2[1,0])))
@@ -57,11 +86,40 @@ enc2 = dct(dec,2)
 # print("{0:b}".format(int(enc2[3,0])))
 
 
-print(enc[0,0])
-print(enc[1,0])
-print(enc[2,0])
-print(enc[3,0])
+print(enc2[0,0])
+#print(enc2[1,0])
+#print(enc2[2,0])
+#print(enc2[3,0])
 print()
+
+print(enc2[0,0]/dec[0,0])
+print(enc[0,0]/img[0,0])
+
+msb1 = "{0:b}".format(int(enc[0,0]))[-8:];
+#msb2 = "{0:b}".format(int(enc[1,0]))[-4:];
+#msb3 = "{0:b}".format(int(enc[2,0]))[-2:];
+#msb4 = "{0:b}".format(int(enc[3,0]))[-2:];
+
+totalMsg = int(msb1,2 ) # + msb2 + msb3 + msb4
+
+print(chr(totalMsg))
+
+msgToEncode(enc, "hello")
+
+    
+
+    #binValue = msg
+#    msb1 = "{0:b}".format(int(enc[0,0]))
+#    # adjust the 2 lsb, 01001000 = H in ascii
+#    enc[0,0] = int(msb1[:-8] + '01001000', 2)
+#    
+#    #decode the encoded picture
+#    return dec = dct(enc,3)
+    
+    
+
+
+# fig.savefig('plot.jpg')
 # print(I)
 # pic = dct(dct(I.T).T)
 # plt.imshow(pic)
