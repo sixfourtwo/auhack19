a2a3a611b0c215bcfe63f2bf78672800aef29bc1
Kasper1337 <au549927@uni.au.dk>
2019-04-07 04:40:14 +0200

    google cloud server up and running

---
+++ PythonServer/app-client.py
@@ -0,0 +1,41 @@
+# importing the requests library 
+import requests 
+import json
+  
+# api-endpoint 
+URL = "https://quantum-spring-236900.appspot.com/water_mark"
+  
+  
+# defining a params dict for the parameters to be sent to the API 
+# data is picture data
+# tagString is the text to embed into picture.
+data = { 
+	"data":"This is the original text",
+	"tagString":" Yesyesyes"
+ }
+
+#f= open("../Images/lego_porche.png","r")
+
+#with open("../Images/lego_porche.png", "rb") as binaryfile :
+#    myArr = bytearray(binaryfile.read())
+
+#strArr = str(myArr)
+
+allData = { "data" : data["data"], 
+			"tagString" : "test tag" }
+data.update(allData)
+
+PARAMS = json.dumps(data)
+
+rPost = requests.post(url = URL, data = PARAMS) # kør det med JSON
+
+data1 = json.loads(rPost.text)
+
+#print("waterMarked data: " + rPost.text )
+print("DATA: \n" + data1["data"])
+
+#finalArr1 = bytes(data1["data"], "utf8")
+#finalArr = bytearray(strArr, "utf8")
+
+#with open("readfile.png", "wb+") as newFile:
+#	newFile.write(finalArr)
---
+++ PythonServer/app-server.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource, reqparse
+import json
+import socket
+
+app = Flask(__name__)
+api = Api(app)
+
+# test Data
+dataJson = [
+	{
+		"data" : "YodaPicData",
+		"tagString" : " no1"
+	},
+	{
+		"data":"carPicData",
+		"tagString" : " no2"
+	}
+]
+
+waterMarkTxt = " pyServ(R) "
+
+class WaterMark(Resource):
+
+	def home(self):
+		return 'Made by Steeaboo at AUHack 2019'
+
+	def post(self):
+		#getting the data in json, load into python object
+		dataIn = json.loads(request.data)
+
+		#Check if data is already present.
+		for myData in dataJson:
+			if( dataIn["data"] == myData["data"] ):
+				return "same data already exists", 400 #Bad request
+
+		dataJson[0].update(dataIn)
+		print("Data to be water marked was submitted")
+
+		#CALL WATERMARKING FUNCTION(s) HERE!
+		WaterMarkedData = dataJson[0]["data"] + dataJson[0]["tagString"]
+		myNewData = { "data" : WaterMarkedData }
+		dataJson[0].update(myNewData)
+
+		print("Returning WaterMarked Data")
+
+		#Return the altered data.
+		return jsonify(dataJson[0])#, 201 #Created - HTTP status code, not working with jsonify..
+
+	#Don't really need these - just here for the view.
+	def get(self):
+		return 0
+
+	def put(self):
+		return 0
+
+	def delete(self):
+		return 0
+
+api.add_resource(WaterMark, "/water_mark")
+
+app.run(debug=True, host="0.0.0.0", port=80)
