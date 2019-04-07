1319833cf50e83b6b3aba8f3fb49329523b0136a
Junge <201305945@post.au.dk>
2019-04-07 02:32:52 +0200

    Now a bit prettier

--- ImageTagger/OutlookTagger/src/components/dragupload/DragUpload.tsx
+++ ImageTagger/OutlookTagger/src/components/dragupload/DragUpload.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react';
-import 'office-ui-fabric-react/dist/css/fabric.min.css';
-import { Spinner } from 'office-ui-fabric-react';
-
+import * as React from "react";
+import "office-ui-fabric-react/dist/css/fabric.min.css";
+import { Spinner } from "office-ui-fabric-react";
 
 /**
  * Heavily based on
@@ -11,129 +10,144 @@ import { Spinner } from 'office-ui-fabric-react';
 export interface DragUploadProps {}
 
 export interface DragUploadState {
-    dragging: Boolean,
-    waiting: Boolean
+  dragging: Boolean;
+  waiting: Boolean;
 }
 
-export default class DragUpload extends React.Component<DragUploadProps, DragUploadState> {
-    
-    // VARIABLES
-    dropRef: any = React.createRef();
-    dragCounter: number;
+export default class DragUpload extends React.Component<DragUploadProps, DragUploadState>{
+  // VARIABLES
+  dropRef: any = React.createRef();
+  dragCounter: number;
 
-    // STATE
-    state = {
-        dragging: false,
-        waiting: false
-    }
+  // STATE
+  state = {
+    dragging: false,
+    waiting: false
+  };
 
-    // LIFECYCLE
-    componentDidMount() {
-        this.dragCounter = 0;
-        let div = this.dropRef.current;
-        div.addEventListener('dragenter', this.handleDragIn);
-        div.addEventListener('dragleave', this.handleDragOut);
-        div.addEventListener('dragover', this.handleDrag);
-        //div.addEventListener('drop', this.handleDrop);
-    }
-
-    componentWillUnmount() {
+  // LIFECYCLE
+  componentDidMount() {
+    this.dragCounter = 0;
     let div = this.dropRef.current;
-    div.removeEventListener('dragenter', this.handleDragIn);
-    div.removeEventListener('dragleave', this.handleDragOut);
-    div.removeEventListener('dragover', this.handleDrag);
+    div.addEventListener("dragenter", this.handleDragIn);
+    div.addEventListener("dragleave", this.handleDragOut);
+    div.addEventListener("dragover", this.handleDrag);
+    //div.addEventListener('drop', this.handleDrop);
+  }
+
+  componentWillUnmount() {
+    let div = this.dropRef.current;
+    div.removeEventListener("dragenter", this.handleDragIn);
+    div.removeEventListener("dragleave", this.handleDragOut);
+    div.removeEventListener("dragover", this.handleDrag);
     //div.removeEventListener('drop', this.handleDrop);
+  }
+
+  // DRAG EVENT HANDLERS
+  handleDrag = e => {
+    // Just need to override default behavior
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  handleDragIn = e => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    this.dragCounter++;
+    this.setState({ dragging: true });
+    if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+      this.setState({ dragging: true });
     }
+  };
 
-    // DRAG EVENT HANDLERS
-    handleDrag = (e) => {
-        // Just need to override default behavior
-        e.preventDefault();
-        e.stopPropagation();
-    };
-    
-    handleDragIn = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        
-        this.dragCounter++;
-        this.setState({dragging: true});
-        if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
-            this.setState({dragging: true});
-          }
-    };
+  handleDragOut = e => {
+    e.preventDefault();
+    e.stopPropagation();
 
-    handleDragOut = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-
-        this.dragCounter--;
-        if(this.dragCounter > 0){
-            return;
-        }
-        this.setState({dragging:false});
-    };
-
-
-    onFileDrop = (e) => {
-      e.stopPropagation();
-      e.preventDefault();
-
-      if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
-        //this.props.handleDrop(e.dataTransfer.files)
-        e.dataTransfer.clearData();
-        this.dragCounter = 0;
-        this.setState({dragging:false})
-        this.setState({waiting: true});
-      }
+    this.dragCounter--;
+    if (this.dragCounter > 0) {
+      return;
     }
+    this.setState({ dragging: false });
+  };
 
+  onFileDrop = e => {
+    e.stopPropagation();
+    e.preventDefault();
 
-    // RENDER
-    render() {
-        return (
+    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+      //this.props.handleDrop(e.dataTransfer.files)
+      e.dataTransfer.clearData();
+      this.dragCounter = 0;
+      this.setState({ dragging: false });
+      this.setState({ waiting: true });
+    }
+  };
+
+  // RENDER
+  render() {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          top: "0",
+          bottom: "0",
+          right: "0",
+          left: "0",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center"
+        }}
+        onDrop={this.onFileDrop}
+        ref={this.dropRef}
+      >
+        {this.state.dragging && (
           <div
-            style={{display: 'block', position: 'absolute', top: '0', bottom: '0', right: '0', left: '0', backgroundColor: 'red'}}
-            onDrop={this.onFileDrop} ref={this.dropRef}>
-            {this.state.dragging &&
-              <div 
-                style={{
-                  border: 'dashed grey 4px',
-                  backgroundColor: 'rgba(255,255,255,.8)',
-                  position: 'absolute',
-                  top: 0,
-                  bottom: 0,
-                  left: 0, 
-                  right: 0,
-                  zIndex: 9999
-                }}
-              >
-                <div 
-                  style={{
-                    position: 'absolute',
-                    top: '50%',
-                    right: 0,
-                    left: 0,
-                    textAlign: 'center',
-                    color: 'grey',
-                    fontSize: 36
-                  }}
-                >
-                  <div>drop here :)</div>
-                </div>
-              </div>
-            }
-            <div style={{ 
-              height: '20%',
-              width: '80%', 
-              borderRadius: '5px',
-              margin: 'auto',
-              textAlign: 'center', 
-              backgroundColor: 'grey'
-            }}>Drag pictures here</div>
-            {this.state.waiting && Spinner}
+            style={{
+              border: "dashed grey 4px",
+              backgroundColor: "rgba(255,255,255,.8)",
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              zIndex: 9999
+            }}
+          >
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                right: 0,
+                left: 0,
+                textAlign: "center",
+                color: "grey",
+                fontSize: 36
+              }}
+            >
+              <div style={{ fontFamily: "sans-serif", fontSize: "20px"}}>DROP HERE</div>
+            </div>
           </div>
-        )
-      }
-
+        )}
+        <div
+          style={{
+            height: "20%",
+            width: "80%",
+            borderRadius: "5px",
+            marginLeft: "auto",
+            marginRight: "auto",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgb(239, 242, 247)"
+          }}
+        >
+          <div style={{textAlign: "center", fontFamily: "sans-serif", fontSize: "20px"}}>Drag pictures here</div>
+          {this.state.waiting && <Spinner style={{marginTop: "30px"}}/>}
+        </div>
+      </div>
+    );
+  }
 }
